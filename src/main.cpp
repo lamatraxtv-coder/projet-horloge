399bdda5fe7d56fb33a450007a41c14d6630504e
arthur Portalier <149090418+lamatraxtv-coder@users.noreply.github.com>
2023-12-14 14:47:16 +0100

    debut du setup pour le menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define largeurMENU 128
+#define hauteurMENU 64
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+//declaration de l'écran connecté en I2C
+Adafruit_SSD1306 display(largeurMENU, hauteurMENU, &Wire,-1);
+void setup(){
+  Serial.begin(115200);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("starting device.");display.print("starting device..");display.print("starting device...");
+  display.display();
+  delay(4000);
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
