b1f596771c57517498354200ff80b7eb5b7c0c70
arthur Portalier <149090418+lamatraxtv-coder@users.noreply.github.com>
2024-01-08 20:07:10 +0100

    reglage bug

--- src/main.cpp
+++ src/main.cpp
@@ -43,11 +43,9 @@ void marche_arret();
 void modeAMPM();
 void reveil24();
 void reveil12();
-void conversion1224();
-void conversion2412();
-void affichertemps();
+/*void affichertemps();
 void effacertemps();
-void startStopTimer(); 
+void startStopTimer();*/ 
 
 Ultrasonic ultrasonic(trigPin, echoPin);
 Adafruit_SSD1306 display(largeurMENU, hauteurMENU, &Wire, -1);
@@ -59,7 +57,7 @@ void setup() {
   pinMode(boutonUP, INPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   
-  attachInterrupt(digitalPinToInterrupt(interruptPin), startStopTimer, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(interruptPin), startStopTimer, CHANGE);
 
   Serial.begin(9600);
 
@@ -156,13 +154,14 @@ void loop() {
   if (compteurflechemenu == 3 && digitalRead(boutonENTER) == HIGH) {
     modeAMPM();
   }
-  if(compteurflechemenu == 4 && digitalRead(boutonENTER) == HIGH){
+  /*if(compteurflechemenu == 4 && digitalRead(boutonENTER) == HIGH){
     affichertemps();
   }
   if(compteurflechemenu == 5 && digitalRead(boutonENTER) == HIGH){
     effacertemps();
-  }
+  }*/
 }
+
 
 void marche_arret() {
   display.clearDisplay();
@@ -183,257 +182,95 @@ void marche_arret() {
   display.display();
 }
 
+
 void modeAMPM() {
   display.clearDisplay();
   display.setCursor(0, 10);
 
-  if (compteurmod == 0) {
-    compteurmod = 1;
-    display.println(F("Changement en format horaire de 24 h"));
-    if(compteurreveilm != 0 && compteurreveilh != 0){
-      conversion1224();
-    }
+  compteurmod = 1 - compteurmod;  // Inversion de l'état
 
-  } else {
-    compteurmod = 0;
-    display.println(F("Changement en format horaire de 12 h"));
-    conversion2412();
-    if(compteurreveilm != 0 && compteurreveilh != 0){
-      conversion2412();
-    }
-  }
+  display.print(F("Changement en format horaire de "));
+  display.println(compteurmod ? F("24 h") : F("12 h"));
+  compteurreveilm=0;
+  compteurreveilh=0;
 
   display.display();
   delay(5000);
 }
 
-void reveil24() {
-  display.clearDisplay();
-  display.setCursor(0, 10);
 
-  while (verifreveil1 == 0) {
-    display.setCursor(0, 10);
-    if (digitalRead(boutonUP) == HIGH) {
-      compteurreveilh++;
-    }
-
-    if (digitalRead(boutonDOWN) == HIGH) {
-      compteurreveilh--;
-    }
-
-    if (compteurreveilh > 23) {
-      compteurreveilh = 0;
-    }
-
-    if (compteurreveilh < 0) {
-      compteurreveilh = 23;
-    }
-
-    display.println(F("heure de la sonnerie  "));
-    display.print(compteurreveilh);
-    display.print(F(":"));
-    display.println(compteurreveilm);
-    display.println(F("appuye sur le bouton central pour selectionner l'heure"));
-    display.display();
-    delay(100);
-    display.clearDisplay();
-
-    if (digitalRead(boutonENTER) == HIGH) {
-      verifreveil1 = 1;
-    }
+void controledutemps(int &value, int minValue, int maxValue) {
+  if (digitalRead(boutonUP) == HIGH) {
+    value = (value + 1) % (maxValue + 1);
   }
 
-  display.setCursor(0, 10);
-
-  while (verifreveil2 == 0) {
-    display.setCursor(0, 10);
-    if (digitalRead(boutonUP) == HIGH) {
-      compteurreveilm++;
-    }
-
-    if (digitalRead(boutonDOWN) == HIGH) {
-      compteurreveilm--;
-    }
-
-    if (compteurreveilm > 59) {
-      compteurreveilm = 0;
-    }
-
-    if (compteurreveilm < 0) {
-      compteurreveilm = 59;
-    }
-
-        display.println(F("heure de la sonnerie  "));
-    display.print(compteurreveilh);
-    display.print(F(":"));
-    display.println(compteurreveilm);
-    display.println(F("appuye sur le bouton central pour selectionner l'heure"));
-    display.display();
-    delay(100);
-    display.clearDisplay();
-    if(digitalRead(boutonENTER)==HIGH){
-      verifreveil2=1;
-    }
+  if (digitalRead(boutonDOWN) == HIGH) {
+    value = (value - 1 + maxValue + 1) % (maxValue + 1);
   }
-  verifreveil1 = 0;
-  verifreveil2 = 0;
 }
 
-void reveil12() {
+void affichage() {
+  display.println(F("heure de la sonnerie  "));
+  display.print(compteurreveilh);
+  display.print(F(":"));
+  display.print(compteurreveilm);
+  
+  if (compteurreveilampm == 0) {
+    display.println(F(" am"));
+  } else {
+    display.println(F(" pm"));
+  }
+
+  display.display();
+  delay(100);
+  display.clearDisplay();
+}
+
+void configureAlarmTime(int &heures, int &minutes, int heureMax, int minuteMax, bool utiliserAmPm = false) {
   display.clearDisplay();
   display.setCursor(0, 10);
 
-  while (verifreveil1 == 0) {
+  while (!verifreveil1) {
     display.setCursor(0, 10);
-    if (digitalRead(boutonUP) == HIGH) {
-      compteurreveilh++;
-    }
-
-    if (digitalRead(boutonDOWN) == HIGH) {
-      compteurreveilh--;
-    }
-
-    if (compteurreveilh > 12) {
-      compteurreveilh = 0;
-    }
-
-    if (compteurreveilh < 0) {
-      compteurreveilh = 12;
-    }
-
-    display.println(F("heure de la sonnerie  "));
-    display.print(compteurreveilh);
-    display.print(F(":"));
-    display.print(compteurreveilm);
-    display.println(etatampm);
-    display.println(F("appuye sur le bouton central pour selectionner l'heure"));
-    display.display();
-    delay(100);
-    display.clearDisplay();
+    controledutemps(heures, 0, heureMax);
+    affichage();
 
     if (digitalRead(boutonENTER) == HIGH) {
       verifreveil1 = 1;
     }
   }
 
-  display.setCursor(0, 10);
-
-  while (verifreveil2 == 0) {
+  while (!verifreveil2) {
     display.setCursor(0, 10);
-    if (digitalRead(boutonUP) == HIGH) {
-      compteurreveilm++;
-    }
+    controledutemps(minutes, 0, minuteMax);
+    affichage();
 
-    if (digitalRead(boutonDOWN) == HIGH) {
-      compteurreveilm--;
-    }
-
-    if (compteurreveilm > 59) {
-      compteurreveilm = 0;
-    }
-
-    if (compteurreveilm < 0) {
-      compteurreveilm = 59;
-    }
-    display.println(F("heure de la sonnerie  "));
-    display.print(compteurreveilh);
-    display.print(":");
-    display.print(compteurreveilm);
-    display.println(etatampm);
-    display.println(F("appuye sur le bouton central pour selectionner l'heure"));
-    display.display();
-    delay(100);
-    display.clearDisplay();
-    if(digitalRead(boutonENTER)==HIGH){
-      verifreveil2=1;
+    if (digitalRead(boutonENTER) == HIGH) {
+      verifreveil2 = 1;
     }
   }
-  while(verifreveil3==0){
 
-    if (digitalRead(boutonUP) == HIGH) {
-      compteurreveilampm++;
-    }
+  if (utiliserAmPm) {
+    while (!verifreveil3) {
+      display.setCursor(0, 10);
+      controledutemps(compteurreveilampm, 0, 1);
+      affichage();
 
-    if (digitalRead(boutonDOWN) == HIGH) {
-      compteurreveilampm--;
+      if (digitalRead(boutonENTER) == HIGH) {
+        verifreveil3 = 1;
+      }
     }
-
-    if (compteurreveilm >1) {
-      compteurreveilampm =  0;
-    }
-
-    if (compteurreveilm < 0) {
-      compteurreveilampm = 1;
-    }
-    if(compteurreveilampm == 0){
-      etatampm= 'am';
-    }
-    if(compteurreveilampm == 1){
-      etatampm ='pm';
-    }
-    display.println(F("heure de la sonnerie  "));
-    display.print(compteurreveilh);
-    display.print(":");
-    display.print(compteurreveilm  );
-    display.println(etatampm);
-    display.println(F("appuye sur le bouton central pour selectionner l'heure"));
-    display.display();
-    delay(100);
-    display.clearDisplay();
-    if(digitalRead(boutonENTER)==HIGH){
-      verifreveil3 = 1;
-    }
-
   }
+
   verifreveil1 = 0;
   verifreveil2 = 0;
   verifreveil3 = 0;
 }
-void conversion1224(){
-  if(compteurreveilampm == 1){
-    compteurreveilh += 12;
-  }
-}
-void conversion2412(){
-  if(compteurreveilh > 12){
-    compteurreveilh=-12;
-    compteurreveilampm = 1; 
-  }
 
-}
-void affichertemps(){
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println("Temps d'extinction:");
-  display.print("   ");
-  display.print(elapsedTime);
-  display.println(" ms");
-  display.display();
-  delay(5000);
+void reveil24() {
+  configureAlarmTime(compteurreveilh, compteurreveilm, 23, 59);
 }
 
-void effacertemps(){
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println("Temps effacé !");
-  display.display();
-  EEPROM.put(MEMORY_ADDRESS, 0); // Efface les données enregistrées dans la mémoire
-  delay(1000);
-}
-void startStopTimer() {
-  if (!started) {
-    started = true;
-    startTime = millis(); // Démarre le chrono
-  } else {
-    stopTime = millis(); // Arrête le chrono
-    started = false;
-    elapsedTime = stopTime - startTime;
-
-    // Enregistrement dans la mémoire EEPROM
-    EEPROM.put(MEMORY_ADDRESS, elapsedTime);
-  }
+void reveil12() {
+  configureAlarmTime(compteurreveilh, compteurreveilm, 12, 59, true);
 }
